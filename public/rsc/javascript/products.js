3e7b480fcec4eaf6dc54312cd1e00ab8875f5e06
erickseis <erickseis@gmail.com>
2022-09-04 18:07:13 -0300

    carga

--- public/rsc/javascript/products.js
+++ public/rsc/javascript/products.js
@@ -2,17 +2,24 @@ import axios from "axios";
 const aplication = document.querySelector("#container-podruct");
 const aplications = document.querySelector("#container-category");
 
-window.getProduct = async (id = 1) => {
 
-  try {
-    const result = await axios.get(
-      `https://bsale-erickseis.vercel.app/api/v1/products/${id}` // ${i} valor dinamico?
-    );
-    const viewData = result.data;
-    console.log(viewData);
-    let cards = ``;
-    viewData.forEach((producCard, indx) => {
-      cards += `
+
+
+
+
+
+
+window.getProduct = async (id) => {
+  if (id = id) {
+    try {
+      const result = await axios.get(
+        `https://bsale-erickseis.vercel.app/api/v1/products/${id}/` // ${i} valor dinamico?
+      );
+      const viewData = result.data;
+      console.log(viewData);
+      let cards = ``;
+      viewData.forEach((producCard, indx) => {
+        cards += `
       
         <div class="card">
         <img src="${producCard.url_image}" class="card-img-top" alt="...">
@@ -29,11 +36,55 @@ window.getProduct = async (id = 1) => {
      
       
       `;
-    });
-    aplication.innerHTML = cards;
-  } catch (error) {
-    console.log(error);
+      });
+      aplication.innerHTML = cards;
+    } catch (error) {
+      console.log(error);
+    }
+  } else if (id = !id) {
+
+    // let btn = document.getElementById('btn')
+
+    function updateView(tname) {
+      try {
+        const result = await axios.get(
+          `https://bsale-erickseis.vercel.app/api/v1/products/name/${tname}` // ${i} valor dinamico?
+        );
+        const viewData = result.data;
+        console.log(viewData);
+        let cards = ``;
+        await viewData.forEach((producCard, indx) => {
+          cards += `
+      
+        <div class="card">
+        <img src="${producCard.url_image}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${producCard.name}</h5>
+        <hr/>
+         <p class="card-text">Price: $ ${producCard.price}</p>
+         
+         <span> 
+         <a class="car-buy" href=""><i class="fa-solid fa-cart-plus"></i></a>
+         </span>
+         </div>
+        </div>
+           `;
+        });
+        aplication.innerHTML = cards;
+      } catch (error) {
+        console.log(error);
+      }
+
+    }
+
+    function greetingOnClickEvent() {
+      const tname = document.getElementById('text').value
+      updateView(tname)
+    }
+    btn.addEventListener("click", greetingOnClickEvent);
+    updateView("world")
   }
+
 };
 
 const getCategory = async () => {
